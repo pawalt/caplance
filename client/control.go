68d80ea7ecf0acf94bf01cf6a2fd75d1a8e385c8
Peyton Walters <pwpon500@gmail.com>
2019-06-22 12:39:53 -0400

    Implement client commands, move communicator to util

--- client/control.go
+++ client/control.go
@@ -1,9 +1,21 @@
 package client
 
-import "net"
+import (
+	"errors"
+	"net"
+	"strings"
+
+	"github.com/pwpon500/caplance/util"
+)
 
 // HealthState represents the current state of the client
 type HealthState int
+
+const (
+	REGISTER_TIMEOUT = 10
+	READ_TIMEOUT     = 20
+	WRITE_TIMEOUT    = 5
+)
 
 const (
 	// Unregistered represents the client state before registration
@@ -14,17 +26,22 @@ const (
 	Active HealthState = 2
 	// Paused represents state when client is registered but packets are not being forwarded
 	Paused HealthState = 3
+	// Deregistering represents state when client is deregistering
+	Deregistering HealthState = 4
 )
 
 // Client holds the current state and configuration for a backend
 type Client struct {
-	dataIP net.IP
-	vip    net.IP
-	state  HealthState
+	dataIP       net.IP
+	vip          net.IP
+	state        HealthState
+	comm         util.Communicator
+	dataListener net.PacketConn
+	name         string
 }
 
 // NewClient creates a new Client object
-func NewClient(dataIP, vip net.IP) *Client {
+func NewClient(vip, dataIP net.IP) *Client {
 	return &Client{
 		dataIP: dataIP,
 		vip:    vip,
@@ -32,6 +49,30 @@ func NewClient(dataIP, vip net.IP) *Client {
 }
 
 // Start attempts to register and listen for connections
-func (c *Client) Start(connectIP net.IP) {
+func (c *Client) Start(connectIP net.IP) error {
 	c.state = Registering
+	conn, err := net.Dial("tcp", connectIP.String()+":1338")
+	if err != nil {
+		return err
+	}
+	c.comm = util.NewTCPCommunicator(conn, READ_TIMEOUT, WRITE_TIMEOUT)
+
+	err = c.comm.WriteLine("REGISTER " + c.name + " " + c.dataIP.String())
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.comm.ReadLine()
+	if err != nil {
+		return err
+	}
+
+	tokens := strings.Split(resp, " ")
+	if tokens[0] != "REGISTERED" {
+		return errors.New(resp)
+	}
+
+	c.state = Paused
+
+	return c.listen()
 }
---
+++ setups/clientSimple.go
@@ -0,0 +1,23 @@
+package setups
+
+import (
+	"log"
+	"net"
+
+	"github.com/pwpon500/caplance/client"
+)
+
+func setupClientSimple() {
+	vip := net.ParseIP("10.0.0.50")
+	dataIP := net.ParseIP("10.0.0.2")
+	c := client.NewClient(vip, dataIP)
+	connectIP := net.ParseIP("10.0.0.1")
+	err := c.Start(connectIP)
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func teardownClientSimple() {
+
+}
